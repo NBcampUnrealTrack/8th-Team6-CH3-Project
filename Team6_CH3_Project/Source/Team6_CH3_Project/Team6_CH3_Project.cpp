6990a7d7a98a871b2d63d75b481eba8bbeb4afd1
Rukyr1 <rlagnscjf123@naver.com>
2026-05-01 12:36:52 +0900

    UProject Created
    챕터3 프로젝트 생성

---
+++ Team6_CH3_Project/Source/Team6_CH3_Project/Team6_CH3_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Team6_CH3_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Team6_CH3_Project, "Team6_CH3_Project" );
