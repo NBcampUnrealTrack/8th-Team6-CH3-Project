7fbc534fd807a10f505950655b0f744527b0f62a
이재호 <jn0796@naver.com>
2026-05-06 19:14:06 +0900

    [item][TP_ThirdPersonCharacter][ThirdPerson]
    - Item : 권총 에셋 추가, Linetrace방식으로 좌클식시 발사, F상호작용키로 권총 획득및 장착(수정 필요)
    - TP_ThirdPersonCharacter : 총기발사, 아이템상호작용 함수 추가
    - ThirdPerson : 블루프린트 추가

--- Source/Team6_CH3_Project/Item/ItemBase.cpp
+++ Source/Team6_CH3_Project/Item/ItemBase.cpp
@@ -1,17 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
+// ItemaBase.h
 #include "ItemBase.h"
 
-// Sets default values
 AItemBase::AItemBase()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
 
-// Called when the game starts or when spawned
 void AItemBase::BeginPlay()
 {
 	Super::BeginPlay();
@@ -25,3 +20,7 @@ void AItemBase::Tick(float DeltaTime)
 
 }
 
+void AItemBase::Interact(ACharacter* Character)
+{
+
+}
--- Source/Team6_CH3_Project/Item/ItemBase.h
+++ Source/Team6_CH3_Project/Item/ItemBase.h
@@ -15,6 +15,9 @@ public:
 	// Sets default values for this actor's properties
 	AItemBase();
 
+	UFUNCTION(BlueprintCallable)
+	virtual void Interact(ACharacter* Character);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
