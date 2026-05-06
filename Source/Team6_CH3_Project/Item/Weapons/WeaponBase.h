c980b1d5c86d772b43d2157ad4a47ca7f178eb96
이재호 <jn0796@naver.com>
2026-05-06 10:37:50 +0900

    [Item][Personal:LJH]
    - Item 폴더 생성및 WeaponBase와 ItemBase추가
    - Personal:LJH 개인 테스트용 폴더 생성

---
+++ Source/Team6_CH3_Project/Item/Weapons/WeaponBase.h
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../ItemBase.h"
+#include "WeaponBase.generated.h"
+
+UCLASS()
+class TEAM6_CH3_PROJECT_API AWeaponBase : public AItemBase
+{
+	GENERATED_BODY()
+	
+public:
+
+
+protected:
+
+
+};
