d20fde82d0b999f0f5392a18026a05a48b351914
문경호\khm99 <khm991008@gmail.com>
2026-05-21 15:12:42 +0900

    [UI]
    무기 슬롯 및 탄약 연동작업 마무리, 폰트 디지털체로 수정, 무기 슬롯 테두리 및 배경색 예시 작업
    회복약 슬롯 제작
    연동 방식을 최대한 블루프린트로 작업했습니다(안정성 및 최적화 측면에서 블루프린트 작업이 더 낫다고 해서요)

---
+++ Source/Team6_CH3_Project/Item/Weapons/SandboxWeaponBase.cpp
@@ -0,0 +1,272 @@
+// SandboxWeaponBase
+#include "SandboxWeaponBase.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/ArrowComponent.h"
+#include "DrawDebugHelpers.h"
+
+void ASandboxWeaponBase::Fire()
+{
+	SandboxFire();
+	ApplyRecoil();
+
+
+	
+	if (OnAmmoChanged.IsBound())
+	{
+		OnAmmoChanged.Broadcast(CurrentAmmo, MaxAmmo);
+	}
+
+
+	CanFire = false;
+	GetWorldTimerManager().SetTimer(
+		TimerFireDelay,
+		this,
+		&ASandboxWeaponBase::HandleFireDelay,
+		RoF,
+		false
+	);
+}
+
+//void ASandboxWeaponBase::Reload()
+//{
+//	if (bIsReloading) return;
+//	if (CurrentAmmo == MaxAmmo) return;
+//	
+//	PlaySound(ReloadStartSound);
+//
+//	bIsReloading = true;
+//	CanFire = false;
+//
+//	float SoundDuration = ReloadStartSound ? ReloadStartSound->GetDuration() : ReloadTime;
+//
+//	GetWorldTimerManager().SetTimer(
+//		TimerReloadDelay,
+//		this,
+//		&ASandboxWeaponBase::FinishReload,
+//		SoundDuration,
+//		false
+//	);
+//}
+
+//void ASandboxWeaponBase::FinishReload()
+//{
+//	CurrentAmmo = MaxAmmo;
+//	bIsReloading = false;
+//	CanFire = true;
+//
+//	PlaySound(ReloadEndSound);
+//}
+
+//bool ASandboxWeaponBase::CheckAmmo()
+//{
+//	if (CurrentAmmo < AmmoPerFire)
+//	{
+//		//PlaySound(EmptySound);
+//		return false;
+//	}
+//	return true;
+//}
+
+bool ASandboxWeaponBase::CanAttack()
+{
+	if (CurrentAmmo < AmmoPerFire || !CanFire)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void ASandboxWeaponBase::LinetraceOneShot(FVector Direction)
+{
+	FVector ViewPointLocation;
+	FRotator ViewPointRotation;
+
+	// 카메라의 현재 위치와 회전값 가져오기
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(ViewPointLocation, ViewPointRotation);
+
+	// ViewStartOffset 만큼 시작 지점을 앞으로 전진
+	ViewPointLocation = ViewPointLocation + ViewPointRotation.Vector() * ViewStartOffset;
+
+	// 시작 지점으로부터 발사 거리(Range)만큼 최종 도달 지점 계산
+	FVector End = ViewPointLocation + ViewPointRotation.Vector() * Range;
+
+	FHitResult HitResult;
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(this);
+	bool bHit = GetWorld()->LineTraceSingleByChannel(
+		HitResult,
+		ViewPointLocation,
+		End,
+		ECC_Visibility,
+		Params
+	);
+
+	// 판정용 트레이스 : 디버그 출력
+	DrawDebugLine(
+		GetWorld(),
+		ViewPointLocation,
+		End,
+		FColor::Red,
+		false,
+		5.f,
+		0,
+		2.0f
+	);
+
+	FVector MuzzleLocation = FirePoint->GetComponentLocation();
+	FVector VisualTraceEnd;
+
+	if (bHit) 
+	{
+		VisualTraceEnd = HitResult.ImpactPoint; 
+	}
+	else 
+	{
+		VisualTraceEnd = HitResult.TraceEnd; 
+	}
+
+	// 연출용 트레이스 : 디버그 출력
+	DrawDebugLine(
+		GetWorld(),
+		MuzzleLocation,
+		VisualTraceEnd,
+		FColor::Blue,
+		false,
+		5.f,
+		0,
+		2.0f
+	);
+
+	// 라인트레이스에 히트시 태그로 대미지 처리
+	if (HitResult.GetActor())
+	{
+		AActor* HitActor = HitResult.GetActor();
+
+		if (HitActor->ActorHasTag(FName(TEXT("Enemy"))))
+		{
+			UGameplayStatics::ApplyDamage(HitActor, DamagePerHit, nullptr, this, UDamageType::StaticClass());
+			UE_LOG(LogTemp, Log, TEXT("%s : %f 피해"), *HitResult.GetActor()->GetActorLabel(), DamagePerHit);
+		}
+
+	}
+}
+
+void ASandboxWeaponBase::LinetraceSpread(FVector Direction, int32 PellectCount, float SpreadAngle)
+{
+	for (int32 i = 0; i < PellectCount; i++)
+	{
+		FVector ViewPointLocation;
+		FRotator ViewPointRotation;
+		// 카메라의 현재 위치와 회전값 가져오기
+		GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(ViewPointLocation, ViewPointRotation);
+
+		// ViewStartOffset 만큼 시작 지점을 앞으로 전진
+		ViewPointLocation = ViewPointLocation + ViewPointRotation.Vector() * ViewStartOffset;
+
+		// 탄 퍼짐(Bullet Spread) 방향(원뿔 모양의 가상 공간(Cone) 안에서 무작위 방향 벡터(VRandCone)를 추출)
+		FVector SpreadDir = FMath::VRandCone(ViewPointRotation.Vector(), FMath::DegreesToRadians(SpreadAngle));
+
+		// 시작 지점으로부터 발사 거리(Range)만큼 최종 도달 지점 계산
+		FVector End = ViewPointLocation + SpreadDir * Range;
+
+		FHitResult HitResult;
+		FCollisionQueryParams Params;
+		Params.AddIgnoredActor(this);
+
+		bool bHit = GetWorld()->LineTraceSingleByChannel(
+			HitResult,
+			ViewPointLocation,
+			End,
+			ECC_Visibility,
+			Params
+		);
+
+
+		// 판정용 트레이스 : 디버그 출력
+		DrawDebugLine(
+			GetWorld(),
+			ViewPointLocation,
+			End,
+			FColor::Red,
+			false,
+			5.f,
+			0,
+			2.0f
+		);
+
+		FVector MuzzleLocation = FirePoint->GetComponentLocation();
+		FVector VisualTraceEnd;
+
+		if (bHit)
+		{
+			VisualTraceEnd = HitResult.ImpactPoint;
+		}
+		else
+		{
+			VisualTraceEnd = HitResult.TraceEnd;
+		}
+
+		// 연출용 트레이스 : 디버그 출력
+		DrawDebugLine(
+			GetWorld(),
+			MuzzleLocation,
+			VisualTraceEnd,
+			FColor::Blue,
+			false,
+			5.f,
+			0,
+			2.0f
+		);
+
+		if (HitResult.GetActor())
+		{
+			AActor* HitActor = HitResult.GetActor();
+
+			if (HitActor->ActorHasTag(FName(TEXT("Enemy"))))
+			{
+				UGameplayStatics::ApplyDamage(HitActor, DamagePerHit, nullptr, this, UDamageType::StaticClass());
+				UE_LOG(LogTemp, Log, TEXT("%s : %f 피해"), *HitResult.GetActor()->GetActorLabel(), DamagePerHit);
+			}
+
+		}
+	}
+}
+
+//void ASandboxWeaponBase::PlaySound(USoundBase* Sound)
+//{
+//	if (Sound)
+//	{
+//		UGameplayStatics::PlaySoundAtLocation(
+//			this,
+//			Sound,
+//			GetActorLocation()
+//		);
+//	}
+//}
+
+void ASandboxWeaponBase::ApplyRecoil()
+{
+	APlayerController* PC = GetWorld()->GetFirstPlayerController();
+	if (PC)
+	{
+		float YawRecoil = FMath::FRandRange(YawRecoilMin, YawRecoilMax);
+		float PitchRecoil = FMath::FRandRange(PitchRecoilMin, PitchRecoilMax);
+
+		PC->AddPitchInput(-PitchRecoil);
+		PC->AddYawInput(YawRecoil);
+	}
+}
+
+void ASandboxWeaponBase::MaxAmmoAdd()
+{
+	CurrentAmmo += MaxAmmo;
+}
+
+
+void ASandboxWeaponBase::UpdateAmmo()
+{
+	CurrentAmmo -= AmmoPerFire;
+}
+
+
---
+++ Source/Team6_CH3_Project/Item/Weapons/WeaponBase.h
@@ -0,0 +1,100 @@
+﻿// WeaponBase.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../ItemBase.h"
+#include "ProjectTypes.h"
+#include "Components\SphereComponent.h"
+#include "WeaponBase.generated.h"
+
+// ─── ⭐여기에 이 한 줄을 새로 추가(복사/붙여넣기) 하세요 ───
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAmmoChangedSignature, int32, CurrentAmmo, int32, MaxAmmo);
+// ─────────────────────────────────────────────────────────
+
+UCLASS()
+class TEAM6_CH3_PROJECT_API AWeaponBase : public AItemBase
+{
+	GENERATED_BODY()
+
+public:
+	AWeaponBase();
+
+	// ─── ⭐여기에 이 두 줄을 새로 추가(복사/붙여넣기) 하세요 ───
+	UPROPERTY(BlueprintAssignable, Category = "Weapon|Delegates")
+	FOnAmmoChangedSignature OnAmmoChanged;
+	// ─────────────────────────────────────────────────────────
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USceneComponent> SceneRoot;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<class UArrowComponent> FirePoint;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* SphereComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Montage")
+	FCharacterAnimMontages CharacterAnimMontages;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponType")
+	EWeaponType WeaponType = EWeaponType::Rifle;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UCameraShakeBase> CameraShakeClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FTransform EquipOffset;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FTransform HolsterOffset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effect")
+	class UNiagaraSystem* MuzzleEffect;
+
+	UFUNCTION(BlueprintCallable)
+	virtual void Fire();
+
+	int32 GetCurrentAmmo() const { return CurrentAmmo; }
+
+	void SetCurrentAmmo(int32 Ammo) { CurrentAmmo = Ammo; }
+
+	UFUNCTION(BlueprintCallable)
+	float GetRoF() const { return RoF; }
+
+	bool GetCanFire() const { return CanFire; }
+
+
+	virtual void Interact(ACharacter* Character) override;
+	virtual void Use(ACharacter* Character) override;
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 AmmoPerFire;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int32 CurrentAmmo;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 MaxAmmo;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float RoF;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Range;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DamagePerHit;
+
+	UPROPERTY(BlueprintReadWrite)
+	bool CanFire;
+
+	UPROPERTY(BlueprintReadWrite)
+	FTimerHandle TimerFireDelay;
+
+	UFUNCTION()
+	void HandleFireDelay();
+};
